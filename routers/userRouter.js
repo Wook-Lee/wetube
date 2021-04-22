5e8ba557712cb09d7973d3de46a0e87fe08a892b
Wook-Lee <ehddnr0525@naver.com>
2021-04-22 18:46:17 +0900

    3.12

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { changePassword, editProfile, userDetail, users } from "../controller/userController";
 import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("User"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req, res) => res.send("Change Password"));
+userRouter.get(routes.home, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;

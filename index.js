d3dfde1975bb6729b5f6e71c0600aff80d0f65ff
Wook-Lee <ehddnr0525@naver.com>
2021-04-22 16:28:21 +0900

    3.7

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hi")
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const  PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+
+// middleware은 시작과 끝 사이에 위차한다.
+// next라는 것을 가진다.
+// 마지막에 next()를 써주지 않으면 끝단에 도착하지 않는다.
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);

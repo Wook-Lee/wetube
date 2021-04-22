6fd5c2cb9d6f33d21d1e1f766d80ceab7d06be29
Wook-Lee <ehddnr0525@naver.com>
2021-04-22 16:28:32 +0900

    3.7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
+
 
 const app = express();
 

a8d9d8f786c4764b30bc5d58099e2d825a981362
Wook-Lee <ehddnr0525@naver.com>
2021-04-23 15:05:10 +0900

    3.16

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,6 @@ import routes from "../routes";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.home, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);
